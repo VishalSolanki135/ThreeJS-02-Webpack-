1dd2c6370164756662ba95a4639f6cc2efc937c0
Vishal Solanki <vishalsolanki14002@gmail.com>
2022-10-15 00:30:06 +0530

    added all files

---
+++ src/script.js
@@ -0,0 +1,6 @@
+// It is the root file and it will be added to the HTML automatically.
+// we can keep static files in static folder
+
+import './style.css'
+import * as THREE from 'three'
+console.log(THREE);
